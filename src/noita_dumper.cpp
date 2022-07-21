8ddeb843ce60f9d676b4f2765fb9549aeb331526
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-07-21 19:47:31 +0200

    Restructuring

--- src/noita_dumper.cpp
+++ src/noita_dumper.cpp
@@ -55,21 +55,21 @@ std::string datetime_string(std::time_t t)
     // 4 +  2 +   2 + 2 +  2 +     2       = 14
     // With separators and '\0'            = 20
 
-    // Year could technically become more digits, so reserve some extra space
-    // for future-proofing.
+    // Year could technically have more digits, so use a larger buffer for future-proofing
     char buffer[99];
     std::strftime(buffer, sizeof(buffer), "%FT%H-%M-%S", std::gmtime(&t));
 
     return buffer;
 }
 
-void dump_noita(DWORD process_id, HANDLE process)
+void dump_process(
+        DWORD process_id, HANDLE process,
+        DWORD thread_id,
+        EXCEPTION_POINTERS* exception_pointers,
+        const char* output_path)
 {
-    auto t = std::time(nullptr);
-    std::string name = "crashes\\minidump_" + datetime_string(t) + ".dmp";
-
     auto dmp_file = CreateFileA(
-        name.c_str(),
+        output_path,
         GENERIC_READ | GENERIC_WRITE,
         FILE_SHARE_WRITE | FILE_SHARE_READ,
         nullptr,
@@ -81,23 +81,7 @@ void dump_noita(DWORD process_id, HANDLE process)
     if (dmp_file == INVALID_HANDLE_VALUE)
         throw_windows_last_error("Couldn't open .dmp file");
 
-    auto dump_info_ptr = int_from_env<std::uintptr_t>("NoitaDumpPTR");
-
-    dump_communication recv;
-    if (!ReadProcessMemory(
-            process,
-            reinterpret_cast<void*>(dump_info_ptr),
-            &recv, sizeof(recv),
-            nullptr)
-    ) {
-        throw_windows_last_error("Couldn't read dump info from Noita process");
-    }
-
-    MINIDUMP_EXCEPTION_INFORMATION exception_info{
-        recv.thread_id,
-        recv.exception_pointers,
-        true
-    };
+    MINIDUMP_EXCEPTION_INFORMATION exception_info{thread_id, exception_pointers, true};
 
     if (!MiniDumpWriteDump(
             process, process_id,
@@ -111,14 +95,15 @@ void dump_noita(DWORD process_id, HANDLE process)
     };
 
     CloseHandle(dmp_file);
-
-    auto dump_finished_event = read_env_handle("NoitaDumpFinishedEvent");
-    SetEvent(dump_finished_event);
 }
 
 void run()
 {
+    auto dump_info_ptr = int_from_env<std::uintptr_t>("NoitaDumpPTR");
+    auto crash_event = read_env_handle("NoitaCrashEvent");
+    auto dump_finished_event = read_env_handle("NoitaDumpFinishedEvent");
     auto process_id = int_from_env<DWORD>("NoitaPID");
+
     auto process = OpenProcess(
         PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | SYNCHRONIZE,
         false, process_id);
@@ -126,27 +111,32 @@ void run()
     if (process == nullptr)
         throw_windows_last_error("Couldn't open Noita process");
 
-    auto crash_event = read_env_handle("NoitaCrashEvent");
-
-    const HANDLE handles[2]{
-        process,
-        crash_event
-    };
-
-    auto wait_result = WaitForMultipleObjects(
-        std::size(handles),
-        handles,
-        false,
-        INFINITE
-    );
+    const HANDLE handles[2]{process, crash_event};
+    auto wait_result = WaitForMultipleObjects(std::size(handles), handles, false, INFINITE);
 
     if (wait_result == WAIT_FAILED)
         throw_windows_last_error("Couldn't wait on dump request or program exit");
 
     auto triggered_handle = handles[wait_result - WAIT_OBJECT_0];
 
-    if (triggered_handle == crash_event)
-        dump_noita(process_id, process);
+    if (triggered_handle == process)
+        return;
+
+    dump_communication recv;
+    if (!ReadProcessMemory(
+            process,
+            reinterpret_cast<void*>(dump_info_ptr),
+            &recv, sizeof(recv),
+            nullptr)
+    ) {
+        throw_windows_last_error("Couldn't read dump info from Noita process");
+    }
+
+    auto t = std::time(nullptr);
+    std::string dump_name = "crashes\\minidump_" + datetime_string(t) + ".dmp";
+
+    dump_process(process_id, process, recv.thread_id, recv.exception_pointers, dump_name.c_str());
+    SetEvent(dump_finished_event);
 }
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
